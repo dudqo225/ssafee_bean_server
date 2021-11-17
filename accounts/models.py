a2457ce0ac7bf1a401adecc4c4d321c6cd4d7e46
dudqo225 <dudqo225@gmail.com>
2021-11-17 14:55:39 +0900

    영화 더미 데이터 추가 & 영화 Model CRUD 로직 작성

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,35 @@ from django.db import models
 
 # User Model 확장
 from django.contrib.auth.models import AbstractUser
+# 이미지 사용
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
 class User(AbstractUser):
-    pass
+    avatar_thumbnail = ProcessedImageField(upload_to='avatars', 
+                                           processors=[ResizeToFill(300, 300)],
+                                           format='JPEG',
+                                           options={'quality': 100})
+    MBTI_CHOICES = [
+        ('NULL', '없음'),
+        ('ISTJ', 'ISTJ'),
+        ('ISTP', 'ISTP'),
+        ('ISFJ', 'ISFJ'),
+        ('ISFP', 'ISFP'),
+        ('INTJ', 'INTJ'),
+        ('INTP', 'INTP'),
+        ('INFJ', 'INFJ'),
+        ('INFP', 'INFP'),
+        ('ESTJ', 'ESTJ'),
+        ('ESTP', 'ESTP'),
+        ('ESFJ', 'ESFJ'),
+        ('ESFP', 'ESFP'),
+        ('ENTJ', 'ENTJ'),
+        ('ENTP', 'ENTP'),
+        ('ENFJ', 'ENFJ'),
+        ('ENFP', 'ENFP'),
+    ]
+    mbti = models.CharField(max_length=10, choices=MBTI_CHOICES, default='없음')
+    pay = models.BooleanField()
+    mileage = models.IntegerField()
 
