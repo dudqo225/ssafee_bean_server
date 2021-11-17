11153923cdd666cf0fe3c3031b9994696bd6c7e9
dudqo225 <dudqo225@gmail.com>
2021-11-17 21:12:42 +0900

    community CRUD 로직 생성

--- community/urls.py
+++ community/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.review_list),
+    path('<int:review_pk>/', views.review_detail),
+    path('comments/', views.comment_list),
+    path('comments/<int:comment_pk>/', views.comment_detail),
+    path('<int:review_pk>/comments/', views.comment_create),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404, get_list_or_404
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
