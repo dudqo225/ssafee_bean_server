2e0b505b8b29a69946a325fcea0677112f659a7f
dudqo225 <dudqo225@gmail.com>
2021-11-23 01:44:14 +0900

    검색 기능 & MBTI 추천 기능 추가

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-# from ..movies.models import UserMovie, Movie
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -8,3 +7,10 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ('id', 'username', 'password', 'avatar_thumbnail', 'mbti', 'pay', 'mileage',)
+    
+class UserUpdateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = get_user_model()
+        fields = ('avatar_thumbnail', 'mbti', 'pay', 'mileage',)
+    
--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,7 @@ from rest_framework import permissions
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
-from .serializers import UserSerializer
+from .serializers import UserSerializer, UserUpdateSerializer
 from django.contrib.auth import get_user_model
 
 
@@ -41,7 +41,7 @@ def user_detail(request, username):
         return Response(serializer.data)
     
     elif request.method == 'PUT':
-        serializer = UserSerializer(user, ata=request.data)
+        serializer = UserUpdateSerializer(user, data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data)
